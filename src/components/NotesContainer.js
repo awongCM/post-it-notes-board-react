0aadc6dd05c995983b97193119058278917caffc
awongCM <awong.cm@gmail.com>
2017-12-01 17:26:28 +1100

    make NotesContainer stateful component; added axios package;

--- src/components/NotesContainer.js
+++ src/components/NotesContainer.js
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
-const NotesContainer = (props) => {
-  return (
-    <h1>NotesContainer</h1>
-  );
-};
+class NotesContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      notes: []
+    };
+  }
+  componentDidMount(){
+    axios.get('http://api.dev.local:3001/v1/notes.json')
+      .then( res => {
+        console.log(res);
+        this.setState({notes: res.data});
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+  render() {
+    return (
+      <div className="NotesContainer">
+        <h1>NotesContainer</h1>
+        <ul>
+          {
+            this.state.notes.map( (note) => {
+              return(
+                <li>
+                  <h4>{note.title}</h4>
+                  <p>{note.content}</p>
+                  <p>{note.color}</p>
+                </li>
+              )
+            })
+          }
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default NotesContainer;
 
