c1995df8ba505899baa66e7ff8422c3784809ac6
awongCM <awong.cm@gmail.com>
2017-12-01 15:19:04 +1100

    added two stateless components

---
+++ src/components/ButtonsContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const ButtonsContainer = (props) => {
+  return (
+    <h1>ButtonsContainer</h1>
+  );
+};
+
+export default ButtonsContainer;
+
+
---
+++ src/components/NotesContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const NotesContainer = (props) => {
+  return (
+    <h1>NotesContainer</h1>
+  );
+};
+
+export default NotesContainer;
+
+
