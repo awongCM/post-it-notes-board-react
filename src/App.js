84af9c959b4b45fba3d6ac0c389696ba116f7374
awongCM <awong.cm@gmail.com>
2017-12-01 13:58:58 +1100

    created stateless componenets; added proptypes package;

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+const App = ({name}) => {
+  return (
+    <h1>Hello World, {name}</h1>
+  );
+};
+
+App.propTypes = {
+  name: PropTypes.string
 }
 
 export default App;
