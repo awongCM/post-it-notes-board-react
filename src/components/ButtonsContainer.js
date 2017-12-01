2c0c0cdb64901952ac0e3f4bcb92a934861baa89
awongCM <awong.cm@gmail.com>
2017-12-01 15:34:53 +1100

    added some simple handleclick event handlers

--- src/components/ButtonsContainer.js
+++ src/components/ButtonsContainer.js
@@ -6,12 +6,16 @@ import './ButtonsContainer.scss';
 const ButtonsContainer = (props) => {
   return (
     <div className="ButtonsContainer">
-      <button>Add New Notes</button>
-      <button>Add New Notes</button>
-      <button>Add New Notes</button>
+      <button onClick={handleClick}>Add New Notes1</button>
+      <button onClick={handleClick}>Add New Notes2</button>
+      <button onClick={handleClick}>Add New Notes3</button>
     </div>
   );
 };
+
+const handleClick = e => (
+  console.log("Button clicked", e.target)
+)
 
 export default ButtonsContainer;
 
