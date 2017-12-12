f8c28e102388d1e443b1cd29b92ecba718df16b6
awongCM <awong.cm@gmail.com>
2017-12-12 15:24:57 +1100

    implemented NoteForm component functionality to add notes

---
+++ src/components/NoteForm.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+
+import './Note.scss';
+
+class NoteForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: this.props.note.title,
+      content: this.props.note.content
+    };
+  }
+  handleInput(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+  handleBlur() {
+    const note = {
+      title: this.state.title,
+      content: this.state.content
+    };
+
+    axios.put(`http://api.dev.local:5000/v1/notes/${this.props.note.id}`,
+      {
+        note: note
+      }
+    )
+    .then(res => {
+      console.log(res);
+      this.props.updateNote(res.data);
+    })
+    .catch(err => console.log(err));
+  }
+
+  render() {
+    const styles = {
+      backgroundColor: this.props.note.color
+    };
+    return(
+      <div className="Note" style={styles}>
+        <form onBlur={this.handleBlur.bind(this)}>
+          <input className="input" 
+                type="text" 
+                name="title" 
+                placeholder="Enter your title"
+                value={this.state.title} onChange={this.handleInput.bind(this)} />
+          <textarea className="textarea" 
+                    name="content" 
+                    placeholder="Enter your content"
+                    value={this.state.content} onChange={this.handleInput.bind(this)}>
+          </textarea>
+        </form>
+      </div>
+    );
+  }
+
+}
+
+export default NoteForm;
+
+
--- src/components/NotesContainer.js
+++ src/components/NotesContainer.js
@@ -5,11 +5,13 @@ import update from 'immutability-helper';
 
 import ButtonsContainer from "./ButtonsContainer";
 import Note from './Note';
+import NoteForm from './NoteForm';
 class NotesContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      notes: []
+      notes: [],
+      editing_note_id:null
     };
   }
   componentDidMount(){
@@ -39,9 +41,16 @@ class NotesContainer extends Component {
       const notes = update(this.state.notes, {
         $splice: [[0, 0, res.data ]]
       });
-      this.setState({notes: notes});
+      this.setState({notes: notes, editing_note_id: res.data.id});
     })
     .catch(err => console.log(err));
+  }
+  updateNote(note) {
+    const note_index = this.state.notes.findIndex( current_note => current_note.id === note.id);
+    const notes = update(this.state.notes, {
+      [note_index]: {$set: note}
+    });
+    this.setState({notes: notes}, () => console.log("All changes saved"));
   }
   render() {
     return (
@@ -50,9 +59,11 @@ class NotesContainer extends Component {
         <h1>NotesContainer</h1>
           {
             this.state.notes.map( (note) => {
-              return(
-                <Note key={note.id} note={note}></Note>
-              )
+              if (this.state.editing_note_id === note.id) {
+                return (<NoteForm key={note.id} note={note} updateNote={this.updateNote.bind(this)}/>)
+              } else {
+                return (<Note key={note.id} note={note} />)
+              }
             })
           }
       </div>
