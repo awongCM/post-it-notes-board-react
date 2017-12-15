15252e08ec423cb69bb68face5ecbceb520cf78c
awongCM <awong.cm@gmail.com>
2017-12-15 12:40:10 +1100

    optimized webpack production build

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,8 +1,12 @@
+const webpack = require('webpack');
 const merge = require('webpack-merge');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.config');
 
 module.exports = merge(common, {
   devtool: 'source-map',
-  plugins: [new UglifyJSPlugin({sourceMap: true})]
+  plugins: [
+    new UglifyJSPlugin({sourceMap: true}), 
+    new webpack.DefinePlugin({ 'process.env.NODE_ENV': JSON.stringify('production')})
+  ]
 });
